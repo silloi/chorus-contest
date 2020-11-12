50d0d1e09ace97ff6335d72342fac2dc1e908412
silloi <silloi1993@gmail.com>
2020-11-13 01:03:06 +0900

    feat: https://content.nuxtjs.org/ja/advanced#%E9%9D%99%E7%9A%84%E3%82%B5%E3%82%A4%E3%83%88%E7%94%9F%E6%88%90

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,4 +44,13 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    },
+  },
 }
